1a5117207710f9faa3da27f93f6c0e673d427629
jalil718@gmail.com <jalil718@gmail.com>
2019-07-23 01:43:49 -0400

    Ant con
    Ant conf

--- src/tests/TotalPageImageCount.java
+++ src/tests/TotalPageImageCount.java
@@ -2,9 +2,9 @@ package tests;
 
 import java.util.List;
 
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
 
 import base.Config;
 
